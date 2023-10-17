836d8555f1b054fc246fa70929a0e78d0aa0dc9b
HhCruZ21 <haizonhelet@gmail.com>
2023-10-17 20:08:28 +0530

    Basic project setup and connection with mongo setup done.

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+require("dotenv").config();
 
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
+const xss = require('xss-clean')
+const mongoSanitize = require('express-mongo-sanitize')
+
+//do the project using local mongoDB. Learn how to bring a hosted mongoDB into this later.
+const mongoURI = process.env.DB_HOST;
+mongoose.connect(mongoURI)
+
+//PARSING
+app.use(bodyParser.json())
+
+//SANITIZE
+app.use(xss())
+app.use(mongoSanitize())
 
 const port = process.env.PORT || 3001;
-app.listen(port, ()=> {
-    console.log(`Server running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
